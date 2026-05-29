a8c48db5de784b0f701abb895ffee645d2727061
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-29 15:37:18 -0600

    Initial Commit

---
+++ AppTracking/AppTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTrackingApp.swift
+//  AppTracking
+//
+//  Created by Alfredo Palacios on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct AppTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTracking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppTracking
+//
+//  Created by Alfredo Palacios on 29/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
