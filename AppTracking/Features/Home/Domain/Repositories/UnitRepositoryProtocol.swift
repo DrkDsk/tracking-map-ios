be39246ef9fa8360db067c81813a137fcfbd1321
Alfredo Palacios <joseaph.1998@gmail.com>
2026-06-01 20:16:16 -0600

    feat: UnitRepository and UnitRepositoryProtocol added

---
+++ AppTracking/Features/Home/Domain/Repositories/UnitRepositoryProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  TrackingRepositoryProtocol.swift
+//  AppTracking
+//
+//  Created by Alfredo Palacios on 29/05/26.
+//
+
+protocol UnitRepositoryProtocol {
+    func getUnits() async throws -> [UnitData]
+}
