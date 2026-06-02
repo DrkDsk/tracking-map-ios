57fa947b1b011ae889259d8448cca05bf10f8a72
Alfredo Palacios <joseaph.1998@gmail.com>
2026-06-01 20:16:28 -0600

    feat: UnitView and UnitViewModel added

---
+++ AppTracking/Features/Home/Presentation/ViewModels/UnitViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  UnitViewModel.swift
+//  AppTracking
+//
+//  Created by Alfredo Palacios on 29/05/26.
+//
+
+import Foundation
+import Combine
+
+@MainActor
+class UnitViewModel: ObservableObject {
+    @Published var units: [UnitData]
+    
+    private let unitRepository: UnitRepositoryProtocol
+    
+    init(unitRepository: UnitRepositoryProtocol) {
+        self.units = []
+        self.unitRepository = unitRepository
+    }
+    
+    func loadUnits() async {
+        do {
+            units = try await unitRepository.getUnits()
+        } catch {
+            print(error)
+        }
+        
+    }
+}
