ed4fbc630d7387af7e1cd5cc356fb7b2d224ca6d
Alfredo Palacios <joseaph.1998@gmail.com>
2026-06-01 19:59:36 -0600

    feat: unit view added

---
+++ AppTracking/Features/Home/Presentation/Views/UnitView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  AppTracking
+//
+//  Created by Alfredo Palacios on 29/05/26.
+//
+
+import SwiftUI
+
+struct UnitView: View {
+    
+    @StateObject private var viewModel = UnitViewModel(unitRepository: AppServiceContainer().unitRepository)
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .task {
+            await viewModel.loadUnits()
+        }
+    }
+}
+
+#Preview {
+    UnitView()
+}
