a3d7ef5febf37f834b1d5707a82023cb3de9fd47
Alfredo Palacios <joseaph.1998@gmail.com>
2026-06-02 20:11:03 -0600

    feat: TokenStorage and LoginRepository added

--- AppTracking/Core/DI/AppServiceContainer.swift
+++ AppTracking/Core/DI/AppServiceContainer.swift
@@ -9,15 +9,14 @@ import Foundation
 import Alamofire
 
 final class AppServiceContainer {
-    lazy var tokenProvider = UserDefaultsTokenProvider()
-    
-    lazy var interceptor = AuthInterceptor(tokenProvider: tokenProvider)
-    
+    lazy var tokenStorage = TokenStorage()
+    lazy var interceptor = AuthInterceptor(tokenStorage: tokenStorage)
     lazy var session = Session(interceptor: interceptor)
-    
     lazy var httpClient : HttpClientProtocol = HttpClient(session: session)
     
     lazy var unitService = UnitService(httpClient: httpClient)
+    lazy var loginService = LoginService(httpClient: httpClient)
     
+    lazy var loginRepository = LoginRepository(service: loginService, tokenStorage: tokenStorage)
     lazy var unitRepository = UnitRepository(service: unitService)
 }
