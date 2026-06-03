0cb42d6c491301752cf455644112a9d9f15c0a05
Alfredo Palacios <joseaph.1998@gmail.com>
2026-06-02 20:11:19 -0600

    feat: LoginRepository and LoginRepositoryProtocol added

---
+++ AppTracking/Features/Login/Data/Repositories/LoginRepository.swift
@@ -0,0 +1,25 @@
+//
+//  LoginRepository.swift
+//  AppTracking
+//
+//  Created by Alfredo Palacios on 02/06/26.
+//
+
+final class LoginRepository : LoginRepositoryProtocol {
+    private let service: LoginServiceProtocol
+    private let tokenStorage: TokenStorage
+    
+    init(service: LoginServiceProtocol, tokenStorage: TokenStorage) {
+        self.service = service
+        self.tokenStorage = tokenStorage
+    }
+    
+    func login(_ email: String, _ password: String) async throws -> LoginData {
+        let dto = try await service.login(email, password)
+        let data = dto
+        
+        try tokenStorage.saveAccessToken(data.accessToken)
+        
+        return data
+    }
+}
