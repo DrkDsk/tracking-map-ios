bf2ca5fcab8045b23bddce6e119223bef9e84cf1
Alfredo Palacios <joseaph.1998@gmail.com>
2026-06-02 20:13:11 -0600

    feat: DashboardSession and LoginViewModel integration in AppCoordinatorView

--- AppTracking/AppCoordinatorView.swift
+++ AppTracking/AppCoordinatorView.swift
@@ -12,14 +12,20 @@ struct AppCoordinatorView: View {
     @Environment(AuthSession.self)
     private var authSession
     
+    @State private var dashboardSession = DashboardSession()
+    
     var body: some View {
         switch authSession.state {
         case .loading:
             EmptyView()
         case .authenticated:
             DashboardCoordinatorView()
+                .environment(dashboardSession)
         case .unauthenticated:
-            LoginView()
+            LoginView(
+                viewModel: LoginViewModel(repository: AppServiceContainer().loginRepository),
+                authSession: authSession
+            )
         }
     }
 }
